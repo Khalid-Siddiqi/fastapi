8510d56a83f89517ad3613b543a82eab878b511f
Khalid <khalid20031016@gmail.com>
2024-12-03 08:22:04 +0500

    done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-from decouple config
+from decouple import config
+from supabase import create_client, Client
+
+url = config("SUPERBASE_URL")
+key = config("SUPERBASE_KEY")
+
+
 app = FastAPI()
+supabase: Client = create_client(url,key)
 
 @app.get("/image")
-async def test():
+async def get_image():
+    image = supabase.table("Patient_image_data")
     return{"msg":"Hello World"}
